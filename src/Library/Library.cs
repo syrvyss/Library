da51dde83b9cd66a21ef18d23483fffd75b9b593
syrvyss <lybkern@gmail.com>
2022-05-21 13:31:25 +0200

    Added loaner get and set methods in library

--- src/Library/Library.cs
+++ src/Library/Library.cs
@@ -9,4 +9,12 @@ class Library {
         return string.Format("Welcome to {0} - the date today is {1}",
             this._libraryName, DateTime.Today.ToString());
     }
+    public Loaner CreateLoaner(int number, string name) {
+        Loaner loaner = new Loaner(number, name);
+        return loaner;
+    }
+    public string GetLoaner(Loaner loaner) {
+        return String.Format("Loaner number: <{0}> - Name: <{1}> is loaner at: <{2}>", 
+        loaner._loanerNumber, loaner._loanerName, this._libraryName);
+    }
 }
