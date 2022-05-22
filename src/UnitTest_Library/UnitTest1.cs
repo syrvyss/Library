66dfc91add68d597395c61c68e038a63d5b140eb
syrvyss <69795518+syrvyss@users.noreply.github.com>
2022-05-22 21:52:27 +0200

    Merge pull request #8 from syrvyss:feature/add_unit_testing (#9)

--- src/UnitTest_Library/UnitTest1.cs
+++ src/UnitTest_Library/UnitTest1.cs
@@ -25,6 +25,6 @@ public class UnitTest1 {
     public void Library_GetLoaner_False() {
         Library lib = new Library("Sonderborg bibliotek");
         lib.CreateLoaner(1, "Adrian");
-        Assert.Equal(lib.GetLoaner(0), "Sorry, wrong index. numbers <= 0 are invalid, index starts at 1");
+        Assert.Equal(lib.GetLoaner(0), "Sorry, duplicate loaner or loaner not found");
     }
 }
