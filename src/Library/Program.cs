d742789cb7f744d4cef537f3a28ba41841ed108f
syrvyss <lybkern@gmail.com>
2022-05-22 14:11:09 +0200

    CreateLoaner method used in Main

--- src/Library/Program.cs
+++ src/Library/Program.cs
@@ -3,6 +3,8 @@
         static void Main() {
             Library lib = new Library("Sonderborg bibliotek");
             Console.WriteLine(lib.GetLibrary());
+            lib.CreateLoaner(1, "Adrian");
+            Console.WriteLine(lib.GetLoaner(1));
         }
     }
 }
